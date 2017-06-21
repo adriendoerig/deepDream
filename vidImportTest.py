2ab2a92f34a1ce2534efa3398223e9fbf18ed36f
adriendoerig <adrien.doerig@gmail.com>
2017-06-21 17:06:33 +0200

    Add files via upload

---
+++ vidImportTest.py
@@ -0,0 +1,8 @@
+import os
+
+imgVidPath = r'C:\Users\doerig\PycharmProjects\deepdream\videoImages'
+
+print os.listdir(imgVidPath)
+
+for i in os.listdir(r"C:\Users\doerig\PycharmProjects\deepdream\videoImages"):
+    print i
